46a8f71365afd82f7d386483cc8d5c1ee2b9592e
gaspary29 <www.macka@mail.ru>
2024-05-18 19:51:46 +0300

    добавил маршруты страниц для импорта

--- helpers.js
+++ helpers.js
@@ -2,6 +2,11 @@ import {getToken, posts} from './index.js';
 import { like, disLike } from './api.js';
 import { renderPostsPageComponent } from './components/posts-page-component.js';
 import { renderUserPostsPageComponent } from './components/user-post-page-component.js';
+import {
+  POSTS_PAGE,
+  USER_POSTS_PAGE,
+} from "./routes.js";
+import { page } from './index.js';
 let appEl = document.getElementById('app');
 export function saveUserToLocalStorage(user) {
   window.localStorage.setItem("user", JSON.stringify(user));
